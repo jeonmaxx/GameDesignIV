4dc0367f3beff89fd1a018fdb61aeac59c645d9c
jeonmaxx <jazuendorf@stud.mediadesign.de>
2023-08-24 22:33:03 +0200

    Cave02

--- Brewbarians/Assets/!Scripts/Puzzle/FieldPuzzle.cs
+++ Brewbarians/Assets/!Scripts/Puzzle/FieldPuzzle.cs
@@ -7,6 +7,9 @@ public class FieldPuzzle : MonoBehaviour
     public List<ClickFieldPuzzle> wrongFields = new List<ClickFieldPuzzle>();
     public List<ClickFieldPuzzle> rightFields = new List<ClickFieldPuzzle>();
     public bool doorOpen;
+    public GameObject[] lights;
+    public GameObject bridgeObj;
+    public Sprite newBridge;
     private bool allRight()
     {
         for (int i = 0; i < rightFields.Count; i++)
@@ -31,8 +34,26 @@ public class FieldPuzzle : MonoBehaviour
         if(allWrong() && allRight())
         {
             doorOpen = true;
+        }
+
+        if(doorOpen)
+        {
             //Sound Abspielen
             //Tür freischalten
+            bridgeObj.GetComponent<SpriteRenderer>().sprite = newBridge;
+            bridgeObj.GetComponent<BoxCollider2D>().enabled = false;
+
+            foreach(GameObject light in lights)
+            {
+                light.SetActive(true);
+            }
+        }
+        else
+        {
+            foreach (GameObject light in lights)
+            {
+                light.SetActive(false);
+            }
         }
     }
 }
